56afb051035b857b4b549252ec01f3079f5a98cb
Andrew Martins <andrew_omartins@yahoo.com>
2021-02-25 09:17:52 -0500

    fixed a bug which was just a spelling nemisis

--- src/index.js
+++ src/index.js
@@ -53,12 +53,13 @@ const Square = (props) => {
               history: [{
                   squares: Array(9).fill(null),
               }],
+              stepNumber: 0,
               xIsNext: true,
           };
       }
 
       handleClick(i) {
-        const history = this.state.history;
+        const history = this.state.history.slice(0,this.state.stepNumber + 1);
         const current = history[history.length - 1];
         const squares = current.squares.slice();
         if (calculateWinner(squares) || squares[i]) {
@@ -68,23 +69,31 @@ const Square = (props) => {
         '👌🏾';
         this.setState({
          history: history.concat([{
-            squares: squares,
+            squares: squares
         }]),
+        stepNumber: history.length,
         xIsNext: !this.state.xIsNext,
       });
+    }
+
+    jumpTo(step) {
+        this.setState({
+            stepNumber: step,
+            xIsNext: (step % 2) === 0,
+        });
     }
     
 
 
     render() {
         const history = this.state.history;
-        const current = history[history.length - 1];
+        const current = history[this.state.stepNumber];
         const winner = calculateWinner(current.squares);
 
         const moves = history.map ((step, move) => {
             const desc = move ?
-            'Got to move #' + move:
-            'Got to game start';
+            'Go to move #' + move:
+            'Go to game start';
         return (
             <li key={move}>
                 <button onClick={() => this.jumpTo(move)}> {desc}
